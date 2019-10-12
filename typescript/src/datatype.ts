a1bc1fc9709f47baa97efb29f661e1b7ac76ae83
sihuanian <2267322300@qq.com>
2019-10-12 19:59:46 +0800

    function array类型

--- typescript/src/datatype.ts
+++ typescript/src/datatype.ts
@@ -15,8 +15,48 @@ console.log(tuple)
 // 函数
 let add = (a:number, b:number) => a + b
 console.log(add(5, 99))
+// => 与 ES6不同，用来表示函数的定义 左边表示参数及类型，右边表示返回值类型
 let compute: (x: number, y: number) => number
 compute = (a, b) => a + b
+console.log(compute(1, 99))
+
+// 重载
+function reverse(x: number): number // 只声明
+function reverse(x: string): number
+function reverse(x: number | string): number | string { // 实现
+  if (typeof x === 'number') {
+    return Number.parseInt(x.toString().split('').reverse().join(''))
+  } else {
+    return x.split('').reverse().join('')
+  }
+}
+
+console.log('reverse("abcdef")', reverse('abcdef'))
+console.log('reverse(123456)', reverse(123456))
+
+// 可选参数 必须放到参数列表的最后
+function minus(x:number, y: number, z?: number): number {
+  let res: number = x
+  for (let i = 1; i < arguments.length; i++) {
+    res -= arguments[i]
+  }
+  return res
+}
+
+console.log('minus(10, 4)', minus(10, 4))
+console.log('minus(10, 4, 2)', minus(10, 4, 2))
+
+function multiply(x: number, y: number, ...items: number[]) {
+  let res: number = x * y
+  items.forEach(item => {
+    res = res * item
+    return res
+  })
+  return res
+}
+console.log('multiply(3, 5)', multiply(3, 5))
+console.log('multiply(3, 5, 4, 6)', multiply(3, 5, 4, 6))
+
 
 // 对象
 let obj: {x: number, y: number} = {x: 1, y: 2}
