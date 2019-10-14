a92e50368ebe20ac04fb5c08bfd21b63236d5890
sihuanian <2267322300@qq.com>
2019-10-14 23:53:38 +0800

    interface

--- typescript/src/interface.ts
+++ typescript/src/interface.ts
@@ -2,16 +2,35 @@ interface Person {
   name: string;
   age: number;
   gender?: string; // 可选的属性
-  [pName: string]: string | number; // 一旦定义了任意属性，那么确定属性和可选属性的类型都必须是它的类型的子集
+  // [pName: string]: string | number; // 一旦定义了任意属性，那么确定属性和可选属性的类型都必须是它的类型的子集
   readonly id: number; // 只读属性
+  print(): void;
 }
 
-let s: Person = {
-  id: 666,
-  name: 'sihuanian',
-  age: 21,
-  gender: 'm',
-  score: 999
-}
+// let s: Person = {
+//   id: 666,
+//   name: 'sihuanian',
+//   age: 21,
+//   gender: 'm',
+//   score: 999
+// }
 
 // s.id = 007 不可以为只读属性赋值
+
+class Stu implements Person {
+  constructor (public id: number, public name: string, public age: number, public gender: string, public score: number, public title: string ) {
+    this.id = id
+    this.name = name
+    this.age = age
+    this.gender = gender
+    this.score = score
+    this.title = title
+  }
+
+  print(): void{
+    console.log(this.name, this.id, this.age)
+  }
+}
+
+const stu = new Stu(2, 'westbook', 30, 'F', 95, 'pg')
+stu.print()
