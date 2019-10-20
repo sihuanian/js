73d8467b8722c6c149339dbcabad58e514c083a9
sihuanian <2267322300@qq.com>
2019-10-20 23:44:37 +0800

    接口

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,4 +1,8 @@
-import './datatype'
+// import './datatype'
+// import './class'
+// import './interface'
+// import './generics.ts'
+import './merge'
 
 let hello: string = 'Hello TypeScript!';
 document.querySelectorAll('.app')[0].innerHTML = hello
--- typescript/src/interface.ts
+++ typescript/src/interface.ts
@@ -7,6 +7,11 @@ interface Person {
   print(): void;
 }
 
+interface Hobbies {
+  hName: string
+  tell (): void
+}
+
 // let s: Person = {
 //   id: 666,
 //   name: 'sihuanian',
@@ -17,20 +22,28 @@ interface Person {
 
 // s.id = 007 不可以为只读属性赋值
 
-class Stu implements Person {
-  constructor (public id: number, public name: string, public age: number, public gender: string, public score: number, public title: string ) {
+class Stu implements Person, Hobbies {
+  public hName: string
+  constructor (public id: number, public name: string, public age: number,
+     public gender: string, public score: number, public title: string, hName: string ) {
     this.id = id
     this.name = name
     this.age = age
     this.gender = gender
     this.score = score
     this.title = title
+    this.hName = hName
   }
 
   print(): void{
     console.log(this.name, this.id, this.age)
   }
+
+  tell (): void {
+    console.log(`my hobbies is ${this.hName}`)
+  }
 }
 
-const stu = new Stu(2, 'westbook', 30, 'F', 95, 'pg')
+const stu = new Stu(2, 'westbook', 30, 'F', 95, 'pg', 'coding')
 stu.print()
+stu.tell()
